705cd893242bfca3bcf2ee55043bd9495601ad9d
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-05-06 10:55:24 -0500

    add subscriptions-transport-ws library and allow to create createWSClient in graphql.js

--- src/common/graphql.js
+++ src/common/graphql.js
@@ -1,8 +1,26 @@
-import { createClient } from 'urql';
+import { createClient, defaultExchanges, subscriptionExchange } from 'urql';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 
 export const client = createClient({
   url: 'https://react.eogresources.com/graphql',
 });
+
+export const createWSClient = () => {
+  const url = 'wss://react.eogresources.com/graphql';
+  const subscriptionClient = new SubscriptionClient(url, { reconnect: true });
+
+  return createClient({
+    url,
+    exchanges: [
+      ...defaultExchanges,
+      subscriptionExchange({
+        forwardSubscription(operation) {
+          return subscriptionClient.request(operation);
+        },
+      }),
+    ],
+  });
+};
 
 export const getWeatherForLocationQuery = `
 query($latLong: WeatherQuery!) {
@@ -18,4 +36,13 @@ export const getMetricsQuery = `
 query {
   getMetrics
 }
+`;
+
+export const newMeasurementSubscription = `
+subscription {
+    newMeasurement {
+      metric
+      value
+    }
+  }
 `;
