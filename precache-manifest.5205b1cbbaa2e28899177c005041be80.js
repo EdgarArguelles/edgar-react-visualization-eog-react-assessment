5967c5f42a5955207be0c94e49d15e63b96094a1
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-05-06 22:16:02 -0500

    Updates

---
+++ precache-manifest.5205b1cbbaa2e28899177c005041be80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7dd468ba1e22f80b9acc0f589b823f1",
+    "url": "/edgar-react-visualization-eog-react-assessment/index.html"
+  },
+  {
+    "revision": "f25c09f092a400462961",
+    "url": "/edgar-react-visualization-eog-react-assessment/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f25c09f092a400462961",
+    "url": "/edgar-react-visualization-eog-react-assessment/static/js/2.25223797.chunk.js"
+  },
+  {
+    "revision": "869fd43a34a23dd8c2f7",
+    "url": "/edgar-react-visualization-eog-react-assessment/static/js/main.2d366535.chunk.js"
+  },
+  {
+    "revision": "4f02c9657cf38ebf709b",
+    "url": "/edgar-react-visualization-eog-react-assessment/static/js/runtime-main.f235ead4.js"
+  }
+]);
